f5096d17634cd8aba08185b7ab20318f9f6dc495
troqy <troqy@example.com>
2023-09-18 17:30:47 +0900

    inertia-vue.js lesson

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,19 @@
+<script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import Label from "@/Components/InputLabel.vue";
+import Input from "@/Components/TextInput.vue";
+// @/Layouts/Guest.vue → @/Layouts/GuestLayout.vue
+// @/Components/Label.vue → @/Components/InputLabel.vue
+// @/Components/Input.vue → @/Components/TextInput.vue
+const emitTest = (e) => console.log(e);
+</script>
+
+<template>
+    <GuestLayout>
+        <Label value="件名">タイトル</Label>
+        <Input
+            modelValue="初期値が入ります"
+            @update:modelValue="emitTest"
+        ></Input>
+    </GuestLayout>
+</template>
---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+
+import { ref } from "vue";
+
+const newTitle = ref("");
+const newContent = ref("");
+</script>
+''' InertiaTest.vue ''' ''' InertiaTest.vue ''' ''' InertiaTest.vue ''' 'abc def
+ghi jkl mno pqr stu vwx yz'
+
+<template>
+    Inertiaテストです。<br />
+    <a href="/">aタグでWelcomeに移動</a><br />
+    <Link href="/">LinkでWelcomeに移動</Link><br />
+    <Link :href="route('inertia.index')">NEW 名前付きルートの確認です</Link
+    ><br />
+    <Link :href="route('inertia.show', { id: 50 })"
+        >ルートパラメータのテストです</Link
+    >
+    <br />
+    <Link :href="route('inertia.show', { id: 5888 })"
+        >ルートパラメータのテストです</Link
+    >
+    <br />
+
+    <div class="mb-8"></div>
+    <input type="text" name="newTitle" v-model="newTitle" />{{ newTitle }}<br />
+    <input type="text" name="newContent" v-model="newContent" />{{ newContent
+    }}<br />
+    <Link
+        as="button"
+        method="post"
+        :href="route('inertia.store')"
+        :data="{
+            title: newTitle,
+            content: newContent,
+        }"
+        >DB保存テスト</Link
+    >
+</template>
